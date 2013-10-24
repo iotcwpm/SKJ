4eb03e770df6392c3a1c551bf1a57189210d5f1c
Nokome Bentley <nokome.bentley@trophia.com>
2013-10-24 16:42:49 +0200

    merge fish.hpp and fishing.hpp into model.hpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 #define TRACKING 1
 
 #include "model.hpp"
+#include "data.hpp"
 
 int main(void){
 	using namespace IOSKJ;
 	Model model;
+
 	model.startup();
+	Data::startup();
+
 	model.simulate();
+
 	model.shutdown();
+	Data::shutdown();
+
 	return 0;
 }
