b15e778db11c0d04216f8f4f981abe359db934b9
Nokome Bentley <nokome.bentley@trophia.com>
2014-03-17 21:33:59 +1300

    Updates Maldives pole and line CPUE to 2012

--- data/cpue-indices.R
+++ data/cpue-indices.R
@@ -4,43 +4,46 @@
 
 # Maldives Pole and Line CPUE
 #
-# Use the "Standardised FAD" index from Table 1 of Sharma et al (2013)
-# Sharma, R, Geehan, J., Adam, M.S. (2013) Maldives Skipjack Pole and Line Fishery Catch Rate Standardization 2004-2011 IOTC-2013-WPTT-15-XX
-# File: IOTC-2013-WPTT-XX - Maldives SKJ CPUE Revisited v5.pdf
+# Uses the "Model 3: Standardized FAD" index from Table 1 of Sharma et al (2014)
+# Sharma, R, Geehan, J., Adam, M.S. (2014) Maldives Skipjack Pole and Line Fishery Catch Rate Standardization 2004-2012: Reconstructing Historic CPUE till 1985. IOTC-2014-WPTT-15-XX
 cat(
 "year	quarter	index
-2004	1	1.22
-2004	2	1.09
-2004	3	1.2
-2004	4	1.41
-2005	1	1.44
-2005	2	1.28
-2005	3	1.41
-2005	4	1.66
+2004	1	1.18
+2004	2	1.05
+2004	3	1.17
+2004	4	1.44
+2005	1	1.33
+2005	2	1.19
+2005	3	1.31
+2005	4	1.63
 2006	1	1.4
-2006	2	1.24
-2006	3	1.38
-2006	4	1.62
-2007	1	1
-2007	2	0.88
-2007	3	0.98
-2007	4	1.15
-2008	1	0.92
-2008	2	0.81
-2008	3	0.9
-2008	4	1.06
-2009	1	0.83
-2009	2	0.73
-2009	3	0.81
-2009	4	0.95
-2010	1	0.66
-2010	2	0.58
-2010	3	0.64
-2010	4	0.76
-2011	1	0.49
-2011	2	0.44
-2011	3	0.49
-2011	4	0.57
+2006	2	1.27
+2006	3	1.4
+2006	4	1.73
+2007	1	0.99
+2007	2	0.89
+2007	3	0.99
+2007	4	1.22
+2008	1	0.98
+2008	2	0.88
+2008	3	0.98
+2008	4	1.23
+2009	1	0.92
+2009	2	0.83
+2009	3	0.92
+2009	4	1.12
+2010	1	0.74
+2010	2	0.66
+2010	3	0.73
+2010	4	0.91
+2011	1	0.57
+2011	2	0.52
+2011	3	0.57
+2011	4	0.7
+2012	1	0.62
+2012	2	0.57
+2012	3	0.62
+2012	4	0.77
 ",
 file = "processed-data/m-pl-cpue.txt"
 )
