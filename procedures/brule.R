bf38a54f2fca88d325c1fb574795933652a627f9
Nokome Bentley <nokome.bentley@trophia.com>
2016-04-01 09:58:50 +1300

    Further refinements

--- procedures/brule.R
+++ procedures/brule.R
@@ -21,12 +21,12 @@ plot <- ggplot(values) +
   geom_rect(xmin=0,xmax=40,ymin=100,ymax=200,fill=hsv(0,0.5,0.9)) +
   geom_rect(xmin=40,xmax=100,ymin=100,ymax=200,fill=hsv(0.15,0.5,0.9)) +
   geom_line(aes(x=status,y=intensity)) +
-  geom_vline(aes(xintercept=safety),lty=2) + 
-  annotate('text',x=safety,y=0,label=paste('Safety limit (X) =',safety),angle=90,vjust=-0.5,hjust=-0.1) +
-  geom_vline(aes(xintercept=threshold),lty=2) + 
-  annotate('text',x=threshold,y=0,label=paste('Threshold level (T) =',threshold),angle=90,vjust=-0.5,hjust=-0.1) +
-  geom_hline(aes(yintercept=imax),lty=2,col='darkgrey') + 
-  annotate('text',x=0,y=imax,label=paste('Maximum fishing intensity (Imax) =',imax),vjust=-0.5,hjust=0,col='#444444') +
+  geom_vline(aes(xintercept=safety),lty=2,col='#444444') + 
+  annotate('text',x=safety,y=0,label=paste('Safety limit (X) =',safety),angle=90,vjust=-0.5,hjust=-0.1,col='#444444') +
+  geom_vline(aes(xintercept=threshold),lty=2,col='#444444') + 
+  annotate('text',x=threshold,y=0,label=paste('Threshold level (T) =',threshold),angle=90,vjust=-0.5,hjust=-0.1,col='#444444') +
+  geom_hline(aes(yintercept=imax),lty=2,col='#444444') + 
+  annotate('text',x=100,y=imax,label=paste('Maximum fishing intensity (Imax) =',imax),vjust=-0.5,hjust=1.1,col='#444444') +
   scale_x_continuous('Stock status (%B0)',limits=c(0,100),expand=c(0,0)) +
   scale_y_continuous('Fishing intensity (%Etarg)',limits=c(0,150),expand=c(0,0)) + 
   labs(colour="")
